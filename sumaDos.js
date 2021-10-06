268a5fc47c30081d338eae5db49f2c33979f4026
Florencia Fregossi <florenciafregossi@gmail.com>
2021-10-06 15:01:51 -0300

    funcion suma refactorizada

--- sumaDos.js
+++ sumaDos.js
@@ -1,3 +1,11 @@
-export const sumaDos = (a, b) => {
+/*export const sumaDos = (a, b) => {
     return a + b
+} */
+
+function sumar(numero1, numero2) {
+    const resultado = numero1 + numero2;
+
+    console.log(resultado);
+
+    return resultado;
 }
